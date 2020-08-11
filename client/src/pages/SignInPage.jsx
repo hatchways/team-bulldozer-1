fe1b86bd527b59b0beb50eb5cafbd26d67c55f70
splantebulldozer <splante@bulldozerinc.com>
2020-08-11 16:41:17 -0400

    Login/signup - Added forms UI and base state

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,10 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import SignedOutPageHeader from '../components/SignedOutPageHeader';
 
-const SignUpPage = () => (
-  <main>
-    <SignedOutPageHeader title="Welcome Back!" subtitle="Login to your account" />
-  </main>
-);
+const useStyles = makeStyles((theme) => ({
+  input: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const SignUpPage = () => {
+  const classes = useStyles();
+
+  const [fields, setFields] = useState({ email: '', password: '' });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (field, event) => {
+    setFields({
+      ...fields,
+      [field]: event.target.value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(fields); // TODO - handle it like there's no tomorrow
+  };
+
+  return (
+    <main>
+      <SignedOutPageHeader title="Welcome Back!" subtitle="Login to your account" />
+      <form onSubmit={handleFormSubmit}>
+        <TextField
+          className={classes.input}
+          id="email"
+          type="email"
+          required
+          label="Your email"
+          variant="outlined"
+          value={fields.email}
+          onChange={(event) => handleChange('email', event)}
+          error={!!errors.email}
+          helperText={errors.email}
+        />
+        <TextField
+          className={classes.input}
+          id="password"
+          type="password"
+          required
+          label="Password"
+          variant="outlined"
+          value={fields.password}
+          onChange={(event) => handleChange('password', event)}
+          error={!!errors.password}
+          helperText={errors.password}
+        />
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </main>
+  );
+};
 
 export default SignUpPage;
--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,10 +1,124 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import SignedOutPageHeader from '../components/SignedOutPageHeader';
 
-const SignUpPage = () => (
-  <main>
-    <SignedOutPageHeader title="Let's Get Started!" subtitle="Create an account" />
-  </main>
-);
+const useStyles = makeStyles((theme) => ({
+  input: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+const SignUpPage = () => {
+  const classes = useStyles();
+
+  const [fields, setFields] = useState({
+    email: '', company: '', password: '', passwordRepeat: '',
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (field, event) => {
+    setErrors({});
+    setFields({
+      ...fields,
+      [field]: event.target.value,
+    });
+  };
+
+  const validate = () => {
+    let isValid = true;
+
+    if (fields.password !== fields.passwordRepeat) {
+      setErrors({
+        ...errors,
+        passwordRepeat: 'Both passwords must match.',
+      });
+      isValid = false;
+    }
+
+    if (fields.password.length < 6) {
+      setErrors({
+        ...errors,
+        password: 'Password must be 6 or more characters long.',
+      });
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (validate()) {
+      console.log(fields); // TODO - handle it like no one's watching
+    }
+  };
+
+  return (
+    <main>
+      <SignedOutPageHeader title="Let's Get Started!" subtitle="Create an account" />
+      <form onSubmit={handleFormSubmit}>
+        <TextField
+          className={classes.input}
+          id="email"
+          type="email"
+          required
+          label="Your email"
+          variant="outlined"
+          value={fields.email}
+          onChange={(event) => handleChange('email', event)}
+          error={!!errors.email}
+          helperText={errors.email}
+        />
+        <TextField
+          className={classes.input}
+          id="company"
+          required
+          label="Company Name"
+          variant="outlined"
+          value={fields.company}
+          onChange={(event) => handleChange('company', event)}
+          error={!!errors.company}
+          helperText={errors.company}
+        />
+        <TextField
+          className={classes.input}
+          id="password"
+          type="password"
+          required
+          label="Password"
+          variant="outlined"
+          value={fields.password}
+          onChange={(event) => handleChange('password', event)}
+          error={!!errors.password}
+          helperText={errors.password}
+        />
+        <TextField
+          className={classes.input}
+          id="passwordRepeat"
+          type="password"
+          required
+          label="Repeat password"
+          variant="outlined"
+          value={fields.passwordRepeat}
+          onChange={(event) => handleChange('passwordRepeat', event)}
+          error={!!errors.passwordRepeat}
+          helperText={errors.passwordRepeat}
+        />
+        <Button
+          className={classes.button}
+          color="primary"
+          variant="contained"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </form>
+    </main>
+  );
+};
 
 export default SignUpPage;
