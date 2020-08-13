7d70eeb120d807be9ad1d3266b4a8669e2d9a265
Ferland <f3rland@gmail.com>
2020-08-13 12:23:33 -0400

    #2 POST /auth/logout added (#23)

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,5 +4,6 @@ const AuthController = require('../controllers/auth');
 module.exports = (app) => {
   app.post('/auth/register', Validators.auth.userSignup, AuthController.SignUp);
   app.post('/auth/login', Validators.auth.userSignin, AuthController.Login);
+  app.post('/auth/logout', AuthController.Logout);
   app.get('/auth/me', AuthController.Self);
 };
