b0855d0f91cf16fd3e8f9fb1634c80cb9cdbe31f
Remi Ferland <f3rland@gmail.com>
2020-08-14 10:05:57 -0400

    First draft for CrawlerService

---
+++ server/src/services/crawler/twitter/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+const twitterCrawler = {};
+twitterCrawler.name = 'twitter';
+twitterCrawler.findPopular = (term) => {
+  return 'twitter';
+};
+twitterCrawler.findRecent = (term) => {
+  return 'twitter';
+};
+
+module.exports = { crawler: twitterCrawler };
