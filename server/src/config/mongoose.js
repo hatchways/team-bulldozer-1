e4e854a5b40c629289f028c7b6c0bd36cdd4d57c
Shums Kassam <skassam21@gmail.com>
2020-08-27 17:04:41 -0400

    let's try remove mongo memory server

--- server/src/config/mongoose.js
+++ server/src/config/mongoose.js
@@ -1,30 +1,30 @@
 /* eslint-disable global-require */
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const config = require('./index').mongo;
+const config = require("./index").mongo;
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
-  connectTimeoutMS: 1000,
+  connectTimeoutMS: 1000
 };
 
-let { uri } = config.uri;
+let { uri } = config;
 
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
   /* eslint-disable import/no-extraneous-dependencies */
-  const { MongoMemoryServer } = require('mongodb-memory-server');
-  const mongoServer = new MongoMemoryServer();
-  (async () => {
-    uri = await mongoServer.getUri();
-    // eslint-disable-next-line no-console
-    mongoose.connect(uri, options, (error) => console.error);
-  })();
-} else {
+  //   const { MongoMemoryServer } = require('mongodb-memory-server');
+  //   const mongoServer = new MongoMemoryServer();
+  //   (async () => {
+  //     uri = await mongoServer.getUri();
+  //     // eslint-disable-next-line no-console
+  //     mongoose.connect(uri, options, (error) => console.error);
+  //   })();
+  // } else {
   // eslint-disable-next-line no-console
-  mongoose.connect(uri, options, (error) => console.error);
+  mongoose.connect(uri, options, error => console.error);
 }
 
 module.exports = mongoose;
