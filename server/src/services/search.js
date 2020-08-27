a8410ff9c6f22c7538d702f49d8ecf51ac34103f
Remi Ferland <f3rland@gmail.com>
2020-08-27 08:35:07 -0400

    ADD Result schema & fix endpoint to read data from DB

--- server/src/services/search.js
+++ server/src/services/search.js
@@ -1,18 +1,10 @@
-const { CrawlerService } = require('./crawler');
-
-const crawlerService = new CrawlerService('');
+const { Result } = require('../models/result');
 
 exports.search = async (req, res, next) => {
-  // TODO: Iterate through active crawlers (crawlerService.getActiveCrawlers()) and combine results
-  const crawler = crawlerService.getCrawler('twitter');
-  const activeCrawlers = crawlerService.getActiveCrawlers(req.user);
-
-  // TODO: Convert to a stardard format
-  if (req.query.type === 'recent') {
-    const result = await crawler.findRecent(req.query.term, req.query.type);
-    res.status(200).send(result);
-  } else {
-    const result = await crawler.findPopular(req.query.term, req.query.type);
-    res.status(200).send(result);
-  }
+  const result = await Result.search(
+    req.query.term,
+    req.user.crawlers,
+    req.query.type || 'popular',
+  );
+  res.status(200).send(result);
 };
